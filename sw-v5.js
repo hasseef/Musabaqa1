dcf9999f1536a68f2369d79d608434c541d3ffc0
hasseef <info@talbiya.sa>
2025-11-09 02:36:08 +0300

    Delete sw-v5.js

--- sw-v5.js
+++
@@ -1,5 +0,0 @@
-const CACHE="musabaqa-pro-v5";
-const ASSETS=["./","./index.html","./assets/css/style.css","./assets/js/main.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
